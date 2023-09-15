85ffef60b2c9eedbda748aff9ad3e662ceec9624
marius-jopen <marius.jopen@thepeople.world>
2023-09-15 11:42:53 +0200

    tailwind css

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,17 +5,17 @@ const defaultTheme = require('tailwindcss/defaultTheme');
 export default {
 	content: ['./src/**/*.{html,js,svelte,ts}'],
 	theme: {
-		screens: {
-			'tablet': '640px',	  
-			'laptop': '1024px',	  
-			'desktop': '1280px',
-			'sm': '640px',
-			'md': '768px',
-			'lg': '1024px',
-			'xl': '1280px',
-			'2xl': '1536px',
-		},
 		extend: {
+			screens: {
+				'tablet': '640px',	  
+				'laptop': '1024px',	  
+				'desktop': '1280px',
+				'sm': '640px',
+				'md': '768px',
+				'lg': '1024px',
+				'xl': '1280px',
+				'2xl': '1536px',
+			},
 			fontFamily: {
 				body: ['inter', ...defaultTheme.fontFamily.sans]
 			},
